b78d9e9283da4409cb7d6406e78670fc837c5c67
adhamamr01 <adhamamr01.aa@gmail.com>
2025-09-18 05:44:21 +0300

    fixed the security issues

--- json.go
+++ json.go
@@ -23,12 +23,17 @@ func respondWithError(w http.ResponseWriter, code int, msg string, logErr error)
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
+
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		// at this point headers are already sent, so we just log the failure
+		log.Printf("Error writing response: %s", err)
+	}
 }
